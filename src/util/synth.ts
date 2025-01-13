62e4584066a98a9a5b14fd70d5ac26d10385c0d9
R4s0n3 <github@miomideal.com>
2025-01-13 21:27:23 +0100

    finetune sound more

--- src/util/synth.ts
+++ src/util/synth.ts
@@ -11,30 +11,30 @@ export async function setupSynth() {
   
   synth = new Tone.PolySynth(Tone.Synth, {
     oscillator: { 
-      type: "triangle1",
+      type: "fatsine1",
     },
     envelope: {
       attack: 0.001,
       decay: 0.05,
-      sustain: 0.3,
+      sustain: 0.123,
       release: 0.1,
     },
-    volume: -30,
+    volume: -20,
   }).toDestination();
 
-  bitCrusher = new Tone.BitCrusher(4).set({
-    wet: 0.8
+  bitCrusher = new Tone.BitCrusher(2).set({ // Increased bit depth from 4 to 8
+    wet: 0.2
   });
 
   reverb = new Tone.Reverb({
-    decay: 0.8,
-    wet: 0.1
+    decay: 0.1,
+    wet: 0.001
   }).toDestination();
 
   delay = new Tone.FeedbackDelay({
     delayTime: "2n",
-    feedback: 0.2,
-    wet: 0.1
+    feedback: 0,
+    wet: 0.01
   });
 
   synth.chain(bitCrusher, delay, reverb);
